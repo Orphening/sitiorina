db5788b1f1e1c5d3886e387add6b0d9b0245ef97
Matías Evert <matias.evert@gmail.com>
2016-08-26 19:54:59 -0300

    Update enviar_correo.php

--- enviar_correo.php
+++ enviar_correo.php
@@ -11,19 +11,19 @@
 	$mail = new PHPMailer();	
 	//Defino los datos del servidor y de conexión
 	//$mail->IsSMTP();
-	$mail->Host     = "mail.rinaruiz.cl";           //Servidor SMTP
-	//$mail->SMTPAuth = true;						  //Autenticación
+	$mail->Host     = "mail.dominio.cl";            //Servidor SMTP
+	//$mail->SMTPAuth = true;			//Autenticación
 	//$mail->SMTPSecure = "ssl";
 	//$mail->Port     = 465;
-	$mail->Username = "info@rinaruiz.cl";         //Usuario
-	$mail->Password = "rina2016";				  //Contraseña
+	$mail->Username = "correo@correo.cl";           //Usuario
+	$mail->Password = "************";	        //Contraseña
 	//Definir Remitente
-	$mail->From     = $correo;   //Correo Remitente
-	$mail->FromName = $nombre;            //Nombre de Remitente
+	$mail->From     = $correo;   			//Correo Remitente
+	$mail->FromName = $nombre;            		//Nombre de Remitente
 	//Definir destinatario(s)
-	$mail->AddAddress("info@rinaruiz.cl",$nombre);
-	//$mail->AddCC('matias.evert@gmail.com', 'ME');  //Copia
-	//$mail->AddBCC('matias.evert@gmail.com', 'ME'); //Copia Oculta
+	$mail->AddAddress("correo@correo.cl",$nombre);
+	//$mail->AddCC('correo@gmail.com', 'ME')	 //Copia
+	//$mail->AddBCC('correo@gmail.com', 'ME'); 	//Copia Oculta
 	
 	//Define los datos técnicos del mensaje
 	$mail->IsHTML(true);  //Define que un email sea enviado como HTML
